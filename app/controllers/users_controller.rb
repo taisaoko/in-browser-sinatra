f25b8d2ca9ac3935215a5c5fb7c52fd16ab91755
taisaoko <ttd3calvin@gmail.com>
2020-03-29 17:12:28 +0000

    update POST method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,14 @@ class UsersController < ApplicationController
   # the purpose of this route is to receive the login form
   # find the user, and log the user in (create a sesson)
   post '/login' do
-    binding.pry
+    # params looks like: {email: "user@user.com", password: "password"}
+    # Find the user
+    @user = User.find_by(email: params[:email])
+    # Authenticate the user - verify the user is who they say they are
+    # they have the credentials - email/password combo
+    @user.authenticate(params[:password])
+    # log the user in
+    # Redirect to user's landing page (show? index? dashboard?)
   end
   # what routes do I need for signup?
   get '/signup' do
