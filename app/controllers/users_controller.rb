125b55f9ce7adad4ea05a785b2d5168a0d4b552c
taisaoko <ttd3calvin@gmail.com>
2020-04-01 19:07:03 +0000

    edit get signup, create user via post and allow logging out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     # undefined method "authenticate" for nil:Nilclass (@user has a value of nil)
     if @user.authenticate(params[:password])
       # log the user in - create the user session
-      session[:user_id] = @user.id #actually loggin the user in
+      session[:user_id] = @user.id # actually logging the user in
       # redirect to user's show page in this case
       puts session
       redirect "/users/#{@user.id}"
@@ -28,13 +28,42 @@ class UsersController < ApplicationController
       # redirect them to the login page
     end
   end
+  
   # what routes do I need for signup?
+  # this route's job is to render a signup form
   get '/signup' do
+    # erb (render a view)
     erb :signup
   end
   
+  post '/users' do
+    # here is where we create a new user and persist the new user to the DB
+    # param will look like this: { "name" => "Elizabeth", "email" => "e@e.com", "password" => 'psw' }
+    # I only want to persist a user that has a name, email, and password
+    if params[:name] !="" && params[:email] != "" && params[:password] != ""
+      # valid input
+      @user = User.create(params)
+      session[:user_id] = @user.id # actually logging the user in
+      # where do I go now?
+      # let's go to the user show page
+      redirect "/users/#{@user.id}" #interpolate b/c it's a string
+    else
+      # not valid input
+      # it would be better to include a message to the user telling what is wrong
+      redirect '/signup'
+    end
+  end
+  
   # user SHOW route
-  get'/users/:id' do
-    "this will be the user show route"
+  get '/users/:id' do
+    # what do I need to do first?
+    # raise params.inspect
+    @user = User.find_by(id: params[:id])
+    erb :'/users/show'
+  end
+  
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 end
