e88dcb5ad34204f5cbd6575f17b4c076a66f7f7f
taisaoko <ttd3calvin@gmail.com>
2020-04-03 21:56:49 +0000

    edit show route and show.erb for a journal entry

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -22,6 +22,9 @@ class JournalEntriesController < ApplicationController
     end
   end
   # show route for a journal entry
-  
+    get '/journal_entries/:id' do
+      @journal_entry = JournalEntry.find(params[:id])
+      erb :'>journal_entries/show'
+    end
   # index route for all journal entries
 end
