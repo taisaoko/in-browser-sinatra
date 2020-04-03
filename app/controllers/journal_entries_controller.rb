1dfdaa1499df2389f80c3631fe6fa55c470bc148
taisaoko <ttd3calvin@gmail.com>
2020-04-03 19:39:56 +0000

    edit post method

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -5,8 +5,20 @@ class JournalEntriesController < ApplicationController
     erb :'/journal_entries/new'
   end
   # post journal_entries to create a new journal entry
-  get '/journal_entries' do
-    erb :'/journal_entries/new'
+  post '/journal_entries' do
+    raise params.inspect
+    # I want to create a new journal entry and save it to the debugger
+    # I also only want to create a journal entry if a user is logged in
+    if !logged_in
+      redirect '/'
+    end
+    # I only want to save the entry if it has some content
+    if params[:content] != ""
+      # create a new entry
+      @journal_entry = JournalEntry.create(content: params[:content], user_id: current_user.id)
+    else
+      redirect '/journal_entries/new'
+    end
   end
   # show route for a journal entry
   
